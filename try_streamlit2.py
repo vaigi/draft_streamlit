0cd0427f5162790c92fbd925b3962b6934ea6a5c
vaigi <94222910+vaigi@users.noreply.github.com>
2024-01-27 20:11:22 +0300

    Update try_streamlit2.py

--- try_streamlit2.py
+++ try_streamlit2.py
@@ -6,4 +6,5 @@ import streamlit as st
 if st.button('Balloons!'):
     st.balloons()
     st.write('1')
-
+number = st.number_input("Insert a number", value=None, placeholder="Type a number...")
+st.write('The current number is ', number)
